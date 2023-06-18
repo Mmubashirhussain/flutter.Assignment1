c90f03191fce6ab91e86253b9710952869410ea1
mmubashirhussain <mh0335320p@gmail.com>
2023-06-19 00:53:32 +0500

    First Assignment upload on Reposatry

---
+++ Q11.dart
@@ -0,0 +1,8 @@
+//import 'dart:io';
+import 'dart:math';
+
+void main() {
+  double num = 25;
+  double squareroot = sqrt(num);
+  print("$squareroot");
+}
---
+++ Q6.dart
@@ -0,0 +1,17 @@
+void main() {
+  String alphabet = 'D';
+  if (alphabet == "A" ||
+      alphabet == "a" ||
+      alphabet == "E" ||
+      alphabet == "e" ||
+      alphabet == "I" ||
+      alphabet == "i" ||
+      alphabet == "O" ||
+      alphabet == "o" ||
+      alphabet == "U" ||
+      alphabet == "u") {
+    print("Alphabet is A Vowel ");
+  } else {
+    print("Not A vowel");
+  }
+}
